df68557a7626a93257a48473bdc77cbe5fa37bac
Logical <88160870+rainbowflood1@users.noreply.github.com>
2026-09-02 21:35:27 -0400

    Background color can be changed
    Included format
    
    Made the y for loop in the TigerUI::Draw function start from 0 and ends at TigerUI::height

--- include/TigerUI/TigerUI.hpp
+++ include/TigerUI/TigerUI.hpp
@@ -7,6 +7,7 @@
 #include <cstdint>
 #include <string>
 #include <vector>
+#include <format>
 
 #define CLEAR (std::string) "\e[2Jm"
 
@@ -17,13 +18,14 @@ public:
 	int height;
 	std::string screen_txt = "";
 	std::vector<uint32_t> screen_color;
-
+	
 	TigerUI() {
 		struct winsize ws;
 		ioctl(0, TIOCGWINSZ, &ws);
 		height = ws.ws_row;
 		width = ws.ws_col;
 		ClearTXT();
+		ClearBGColor();
 	}
 	void Text(std::string str, uint32_t x, uint32_t y) {
 		for (uint32_t i = 0; i < str.length(); i++) {
@@ -36,18 +38,25 @@ public:
 			screen_txt += " ";
 		}
 	}
+	void ClearBGColor() {
+		screen_color.clear();
+		for (uint32_t i = 0; i < width*height; i++) {
+			screen_color.push_back(0x000000FF);
+		}
+	}
 	void Draw() {
 		std::cout << CLEAR;
 		std::string screen = "";
 
-
-		for (uint32_t y = height; y < 0; y--) {
+		for (uint32_t y = 0; y < height; y--) {
 			for (uint32_t x = 0; x < width; x++) {
-				screen += screen_txt[Location(x, y)];
+				uint8_t R = (screen_color[Location(x, y)] >> 24) & 0xFF;
+				uint8_t G = (screen_color[Location(x, y)] >> 16) & 0xFF;
+				uint8_t B = (screen_color[Location(x, y)] >> 8) & 0xFF;
+				screen += (std::format("\e[48;2;{};{};{}m", static_cast<uint32_t>(R), static_cast<uint32_t>(G), static_cast<uint32_t>(B)) + screen_txt[Location(x, y)]);
 			}
 			screen += "\n";
 		}
-
 
 		std::cout << screen;
 	}
