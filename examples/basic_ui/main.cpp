f1c2cbbeea4025e6b65a3b655e4116139c9e4ec5
rainbowflood1 <88160870+rainbowflood1@users.noreply.github.com>
2026-09-03 18:38:11 +0000

    Added box where the text inside changes color, added text about what commands there are

--- examples/basic_ui/main.cpp
+++ examples/basic_ui/main.cpp
@@ -3,13 +3,17 @@
 TigerUI tui;
 
 void command(std::string command) {
+	// Now you can clear and update elements
 	tui.Draw();
 }
 
 int main() {
+	// Elements that do not update
 	tui.DrawBGBox(0.1f, 0.1f, 0.9f, 0.9f, 0x1A1A1AFF);
 	tui.DrawTextColorBox(0.1f, 0.1f, 0.9f, 0.9f, 0x1A1AFFFF);
 	tui.Text("Hello world!", 0.5, 0.5);
+	tui.DrawTextColorBox(0.6f, 0.1f, 0.9f, 0.9f, 0xFFFFFFFF);
+	tui.Text("Commands to use:", 0.7, 0.2);
 	tui.CommandListener(&command);
 
 	return 0;
