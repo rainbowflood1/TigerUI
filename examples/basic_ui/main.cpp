73d49b7c0e28570517567d5631bf7f1c2ae3c84b
Logical <88160870+rainbowflood1@users.noreply.github.com>
2026-09-05 15:05:35 -0400

    Changed the background of the basic_ui example, organized everything and added comments

--- examples/basic_ui/main.cpp
+++ examples/basic_ui/main.cpp
@@ -9,11 +9,19 @@ void command(std::string command) {
 
 int main() {
 	// Elements that do not update
+	
+	// Change the background
+	tui.BG = 0x101010FF;
+	tui.ClearBGColor();
+	// Change the background in a area
 	tui.DrawBGBox(0.1f, 0.1f, 0.9f, 0.9f, 0x1A1A1AFF);
+	// Change the text color in an area
 	tui.DrawTextColorBox(0.1f, 0.1f, 0.9f, 0.9f, 0x1A1AFF00);
-	tui.Text("Hello world!\nHello world!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!", 0.5, 0.5);
 	tui.DrawTextColorBox(0.6f, 0.1f, 0.9f, 0.9f, 0xFFFFFF01);
+	// Draw text
+	tui.Text("Hello world!\nHello world!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!", 0.5, 0.5);
 	tui.Text("Commands to use:\nThere are no commands available right now.", 0.8, 0.2);
+	// Listen for commands
 	tui.CommandListener(&command);
 
 	return 0;
