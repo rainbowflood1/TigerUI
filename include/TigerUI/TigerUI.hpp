9dd9caac99734fc85302e16c3d21c406135b36f8
Logical <88160870+rainbowflood1@users.noreply.github.com>
2026-09-05 18:10:41 -0400

    Removed includes for format and functions defined

--- include/TigerUI/TigerUI.hpp
+++ include/TigerUI/TigerUI.hpp
@@ -7,7 +7,6 @@
 #include <cstdint>
 #include <string>
 #include <vector>
-//#include <format>
 #include <functional>
 #include <sstream>
 #include <cmath>
@@ -134,8 +133,6 @@ public:
 				uint8_t TXT_B = (screen_text_color[cell_location_index] >> 8) & 0xFF;
 				uint8_t TXT_TYPE = screen_text_color[cell_location_index] & 0xFF;
 
-				//screen += (std::format("\e[48;2;{};{};{}m", static_cast<uint32_t>(BG_R), static_cast<uint32_t>(BG_G), static_cast<uint32_t>(BG_B)) + std::format("\e[38;2;{};{};{}m", static_cast<uint32_t>(TXT_R), static_cast<uint32_t>(TXT_G), static_cast<uint32_t>(TXT_B)) + screen_txt[Location(x, y)]);
-				
 				screen += "\e[" + std::to_string(TXT_TYPE) + "m" + (std::string) "\e[48;2;" + std::to_string(BG_R) + (std::string) ";" + std::to_string(BG_G) + (std::string) ";" + std::to_string(BG_B) + (std::string) "m" + (std::string) "\e[38;2;" + std::to_string(TXT_R) + (std::string) ";" + std::to_string(TXT_G) + (std::string) ";" + std::to_string(TXT_B) + (std::string) "m" + screen_txt[Location(x, y)];
 			}
 			screen += "\n";
